0ebd6412fe71f5cc83e056960915ae7bfc422018
SNA <snalahakoon299@gmail.com>
2025-03-06 14:55:06 +0530

    Adeed sustractNumbers method

--- Calculator.java
+++ Calculator.java
@@ -4,8 +4,17 @@ public class Calculator {
         return a + b;
     }
 
+    // Method to subtract two numbers
+    public static int subtractNumbers(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
-        int result = addNumbers(5, 3);
+        int result1 = addNumbers(5, 3);
         System.out.println("The sum is: " + result);
+
+        int result2 = subtractNumbers(10, 4);
+        System.out.println("The difference is: " + result2);
+    
     }
 }
